0dabe5cc2a003a88da12b924757bdec0fc31e476
Guilherme Penso <guilhermepenso@gmail.com>
2023-12-21 06:51:52 -0300

    dev-repo(update_0.9.1)
    Starting creation of repository filtered by user_id.

--- react-node/dev-repo-server/src/controllers/RepositoriesController.js
+++ react-node/dev-repo-server/src/controllers/RepositoriesController.js
@@ -4,8 +4,19 @@ import Repository from "../models/Repository";
 class RepositoriesController {
     async index(req, res) {
         try {
-            const users = await User.find();
-            return res.json(users);
+            const { user_id } = req.params;
+
+            const user = await User.findById(user_id);
+
+            if (!user) {
+                return res.status(404).json();
+            }
+
+            const repositories = await Repository.find({
+                userId: user_id
+            });
+
+            return res.json(repositories);
         }   
         catch(err) {
             console.error(err);
@@ -13,6 +24,27 @@ class RepositoriesController {
                 .status(500)
                 .json({ error: 'Internal server error' });
         } 
+    }
+
+    async create(req, res) {
+        try {
+            const { user_id } = req.params;
+
+            const user = await User.findById(user_id);
+
+            if (!user) {
+                return res.status(404).json();
+            }
+
+            
+
+        }   
+        catch(err) {
+            console.error(err);
+            return res
+                .status(500)
+                .json({error: 'Internal server error'});
+        }     
     }
 }
 
